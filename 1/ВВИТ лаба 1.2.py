a382e028e6369b930fdf518df7b309e9a1e2d11e
unknown <ovezovumid00@gmail.com>
2024-12-06 14:52:21 +0300

    Lab1

---
+++ 1/ВВИТ лаба 1.2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Input first number"))
+num2 = int(input("Input second number"))
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
+else:
+    print("The numbers are equal")
